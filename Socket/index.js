3b39d63a079395c48be646b6b32ad74b1783f392
KelpinHarr <calvinharsono07@gmail.com>
2023-12-13 08:45:17 +0700

    benerin socket

--- Socket/index.js
+++ Socket/index.js
@@ -5,12 +5,27 @@ const io = new Server({ cors: "http://localhost:5173" });
 let onlineUser = []
 
 io.on("connection", (socket) => {
+    socket.on("addNewUser", (userId) => {
+        !onlineUser.some((o) => o.userId === userId) && 
+            onlineUser.push({
+                userId,
+                socketId: socket.id
+            });
+        io.emit("getOnlineUser", onlineUser);
+    })
+
     socket.on("sendMessage", (message) => {
         const user = onlineUser.find((u) => u.userId === message.recipientId);
 
         if (user){
             io.to(user.socketId).emit("getMessage", message);
         }
+    })
+
+    socket.on("disconnect", () => {
+        onlineUser = onlineUser.filter((o) => o.socketId !== socket.id);
+
+        io.emit("getOnlineUser", onlineUser);
     })
 })
 
