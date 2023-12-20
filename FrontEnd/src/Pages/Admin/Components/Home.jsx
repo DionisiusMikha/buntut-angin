c6dd142ce333b6d0787c6ac36208c26ae21cbd74
Dionisius Mikha <Vitoutama82@gmail.com>
2023-12-20 18:20:44 +0700

    petot at reso 1080p chart admin fixed

--- FrontEnd/src/Pages/Admin/Components/Home.jsx
+++ FrontEnd/src/Pages/Admin/Components/Home.jsx
@@ -39,12 +39,12 @@ function Home() {
                         </div>
                         <div className="grid grid-cols-2 py-3">
                             <dl>
-                            <dt className="text-base font-normal text-gray-500 -400 pb-1">Income</dt>
-                            <dd className="leading-none text-xl font-bold text-green-500 -400">$23,635</dd>
+                            <dt className="text-base font-normal text-gray-500 -400 pb-1">New Subscription</dt>
+                            <dd className="leading-none text-xl font-bold text-green-500 -400">4 User</dd>
                             </dl>
                             <dl>
-                            <dt className="text-base font-normal text-gray-500 -400 pb-1">Expense</dt>
-                            <dd className="leading-none text-xl font-bold text-red-600 -500">-$18,230</dd>
+                            <dt className="text-base font-normal text-gray-500 -400 pb-1">Lose Subscription</dt>
+                            <dd className="leading-none text-xl font-bold text-red-600 -500">2 User</dd>
                             </dl>
                         </div>
                         <div id="bar-chart"></div>
@@ -98,9 +98,9 @@ function Home() {
                             </div>
                         </div>
                 </div>
-                <div className="bg-white w-full mt-8 h-full rounded-xl drop-shadow-md px-5 py-5">
+                <div className="bg-white w-full mt-8 h-full p rounded-xl drop-shadow-md px-5 py-9">
                     <div className="text-3xl font-semibold">Web Analytics</div>
-                    <div className="max-w-xxxxl w-full max-h-fit bg-white rounded-lg shadow -800 p-4 md:p-6">
+                    <div className="max-w-xxxxl w-full h-fit bg-white rounded-lg shadow -800 p-4 md:p-6">
                         <div className="flex justify-between">
                             <div>
                             <h5 className="leading-none text-3xl font-bold text-gray-900  pb-2">32.4k</h5>
@@ -115,19 +115,8 @@ function Home() {
                             </div>
                         </div>
                         <div id="area-chart"></div>
-                        <div className="grid grid-cols-1 items-center border-gray-200 border-t -700 justify-between">
+                        <div className="grid grid-cols-1 items-center border-gray-200 border-t justify-between h-1">
                             <div className="flex justify-between items-center pt-5">
-                            <button
-                                id="dropdownDefaultButton"
-                                data-dropdown-toggle="lastDaysdropdown"
-                                data-dropdown-placement="bottom"
-                                className="text-sm font-medium text-gray-500 -400 hover:text-gray-900 text-center inline-flex items-center -white"
-                                type="button">
-                                Last 7 days
-                                <svg className="w-2.5 m-2.5 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
-                                </svg>
-                            </button>
                             <div id="lastDaysdropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 -700">
                                 <ul className="py-2 text-sm text-gray-700 -200" aria-labelledby="dropdownDefaultButton">
                                     <li>
@@ -150,13 +139,15 @@ function Home() {
                             <a
                                 href="#"
                                 className="uppercase text-sm font-semibold inline-flex items-center rounded-lg text-blue-600 hover:text-blue-700 -blue-500  hover:bg-gray-100 -gray-700 -gray-700 -700 px-3 py-2">
-                                Users Report
+                                Analytics Report
                                 <svg className="w-2.5 h-2.5 ms-1.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
                                 </svg>
                             </a>
                             </div>
+
                         </div>
+
                         </div>
                 </div>
            </div>
